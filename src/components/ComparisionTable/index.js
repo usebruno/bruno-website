0a7d627ec99bf37a3cd3d8989d0e60c8551e6533
Anoop M D <anoop.md1421@gmail.com>
2022-10-17 02:40:33 +0530

    feat: added google analytics

--- src/components/ComparisionTable/index.js
+++ src/components/ComparisionTable/index.js
@@ -43,6 +43,23 @@ const ComparisionTable = () => {
           </tr>
           <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
             <td scope="row" className="py-2 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              Opensource Sync Server
+            </td>
+            <td className="py-2 px-6">
+              ✔️
+            </td>
+            <td className="py-2 px-6">
+              ✖️
+            </td>
+            <td className="py-2 px-6">
+              ✖️
+            </td>
+            <td className="py-2 px-6">
+              ✖️
+            </td>
+          </tr>
+          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <td scope="row" className="py-2 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
               Local (Filesystem) Collections
             </td>
             <td className="py-2 px-6">
@@ -104,23 +121,6 @@ const ComparisionTable = () => {
             </td>
             <td className="py-2 px-6">
               ✔️
-            </td>
-            <td className="py-2 px-6">
-              ✖️
-            </td>
-          </tr>
-          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <td scope="row" className="py-2 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              Opensource Sync Server
-            </td>
-            <td className="py-2 px-6">
-              ✔️
-            </td>
-            <td className="py-2 px-6">
-              ✖️
-            </td>
-            <td className="py-2 px-6">
-              ✖️
             </td>
             <td className="py-2 px-6">
               ✖️
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -33,6 +33,23 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet"/>
+          {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS && <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />}
+
+          {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS && <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                  page_path: window.location.pathname,
+                });
+              `
+            }}
+          />}
         </Head>
         <body>
           <Main />
