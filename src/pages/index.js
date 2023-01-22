774ec007b6652fa1de73c5f25bb2da1bfbabe0d5
Anoop M D <anoop.md1421@gmail.com>
2023-01-22 06:01:13 +0530

    chore: fixed lint issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,18 +17,18 @@ export default function Home() {
       <main className="flex flex-grow flex-col notebase-app px-4">
         <Navbar />
 
-        <section class="bg-white dark:bg-gray-900">
-          <div class="max-w-screen-xl pt-8 pb-8 mx-auto lg:py-12 lg:pt-20">
-            <div class="mr-auto place-self-center">
-              <h1 class="max-w-3xl mb-4 text-4xl font-bold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">Stop searching for lost api collections.</h1>
-              <p class="max-w-3xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+        <section className="bg-white dark:bg-gray-900">
+          <div className="max-w-screen-xl pt-8 pb-8 mx-auto lg:py-12 lg:pt-20">
+            <div className="mr-auto place-self-center">
+              <h1 className="max-w-3xl mb-4 text-4xl font-bold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">Stop searching for lost api collections.</h1>
+              <p className="max-w-3xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
                 Developers will no longer ask "Can you share a postman/x collection for this service <br/><br/>
                 It will reside where it was destined to reside.<br/>
                 Alongside your source code.
               </p>
-              <div class="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-                <a href="https://github.com/usebruno/bruno" class="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+              <div className="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+                <a href="https://github.com/usebruno/bruno" className="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
                     <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
                   </svg>
                   <span className='ml-3'>View On Github</span>
@@ -38,13 +38,13 @@ export default function Home() {
           </div>
         </section>
 
-        <section class="bg-white dark:bg-gray-900">
-          <div class="max-w-screen-xl pt-10 pb-8 mx-auto lg:py-16 lg:pt-10">
-            <div class="mr-auto place-self-center">
-              <h1 class="max-w-3xl mb-4 text-4xl font-bold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
+        <section className="bg-white dark:bg-gray-900">
+          <div className="max-w-screen-xl pt-10 pb-8 mx-auto lg:py-16 lg:pt-10">
+            <div className="mr-auto place-self-center">
+              <h1 className="max-w-3xl mb-4 text-4xl font-bold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
                 Introducing Bruno
               </h1>
-              <p class="max-w-3xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+              <p className="max-w-3xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
                 An Opensource IDE for exploring and testing Api's <br/><br/>
                 
                 Previous attempts at integrating API collections into source code have fallen short, hindered by clunky user interfaces and inadequate file designs. <br /><br />
@@ -54,7 +54,7 @@ export default function Home() {
           </div>
         </section>
 
-        {/* <section class="bg-gray-50 dark:bg-gray-800">
+        {/* <section className="bg-gray-50 dark:bg-gray-800">
           Hello
         </section> */}
 
