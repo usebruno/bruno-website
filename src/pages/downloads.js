36e998c617895b9fb9aff24f01913188f5ec12a5
Anoop M D <anoop.md1421@gmail.com>
2023-02-07 08:39:57 +0530

    chore: release v0.9.0

--- src/pages/downloads.js
+++ src/pages/downloads.js
@@ -38,7 +38,7 @@ export default function Downloads() {
                   <IconBrandApple className="text-gray-500" size={24} strokeWidth={2}/><span className="label ml-2">Mac x64</span>
                 </td>
                 <td className="py-3 pl-6 pr-10">
-                <a href="https://github.com/usebruno/bruno/releases/download/v0.8.2/bruno_0.8.2_x64_mac.dmg" target="_blank" rel="noreferrer" className='link'>
+                <a href="https://github.com/usebruno/bruno/releases/download/v0.9.0/bruno_0.9.0_x64_mac.dmg" target="_blank" rel="noreferrer" className='link'>
                   Download
                 </a>
                 </td>
@@ -49,7 +49,7 @@ export default function Downloads() {
                   <IconBrandApple className="text-gray-500" size={24} strokeWidth={2}/><span className="label ml-2">Mac arm64</span>
                 </td>
                 <td className="py-3 pl-6 pr-10">
-                <a href="https://github.com/usebruno/bruno/releases/download/v0.8.2/bruno_0.8.2_arm64_mac.dmg" target="_blank" rel="noreferrer" className='link'>
+                <a href="https://github.com/usebruno/bruno/releases/download/v0.9.0/bruno_0.9.0_arm64_mac.dmg" target="_blank" rel="noreferrer" className='link'>
                   Download
                 </a> <br />
                 <span style={{fontSize: 12}}>
@@ -64,7 +64,7 @@ export default function Downloads() {
                   <IconBrandUbuntu className="text-gray-500" size={24} strokeWidth={2}/><span className="label ml-2">Ubuntu Linux</span>
                 </td>
                 <td className="py-3 pl-6 pr-10">
-                <a href="https://github.com/usebruno/bruno/releases/download/v0.8.2/bruno_0.8.2_amd64_linux.deb" target="_blank" rel="noreferrer" className='link'>
+                <a href="https://github.com/usebruno/bruno/releases/download/v0.9.0/bruno_0.9.0_amd64_linux.deb" target="_blank" rel="noreferrer" className='link'>
                   Download
                 </a>
                 </td>
@@ -75,7 +75,7 @@ export default function Downloads() {
                   <IconDeviceDesktop className="text-gray-500" size={24} strokeWidth={2}/><span className="label ml-2">Linux</span>
                 </td>
                 <td className="py-3 pl-6 pr-10">
-                <a href="https://github.com/usebruno/bruno/releases/download/v0.8.2/bruno_0.8.2_x86_64_linux.AppImage" target="_blank" rel="noreferrer" className='link'>
+                <a href="https://github.com/usebruno/bruno/releases/download/v0.9.0/bruno_0.9.0_x86_64_linux.AppImage" target="_blank" rel="noreferrer" className='link'>
                   Download
                 </a>
                 </td>
@@ -86,7 +86,7 @@ export default function Downloads() {
                   <IconBrandWindows className="text-gray-500" size={24} strokeWidth={2}/><span className="label ml-2">Windows x64</span>
                 </td>
                 <td className="py-3 pl-6 pr-10">
-                <a href="https://github.com/usebruno/bruno/releases/download/v0.8.2/bruno_0.8.2_x64_win.exe" target="_blank" rel="noreferrer" className='link'>
+                <a href="https://github.com/usebruno/bruno/releases/download/v0.9.0/bruno_0.9.0_x64_win.exe" target="_blank" rel="noreferrer" className='link'>
                   Download
                 </a>
                 </td>
@@ -99,7 +99,7 @@ export default function Downloads() {
                   <IconBrandWindows className="text-gray-500" size={24} strokeWidth={2}/><span className="label ml-2">Windows x64 (zip with dll)</span>
                 </td>
                 <td className="py-3 pl-6 pr-10">
-                <a href="https://github.com/usebruno/bruno/releases/download/v0.8.2/bruno_0.8.2_x64_win.zip" target="_blank" rel="noreferrer" className='link'>
+                <a href="https://github.com/usebruno/bruno/releases/download/v0.9.0/bruno_0.9.0_x64_win.zip" target="_blank" rel="noreferrer" className='link'>
                   Download
                 </a>
                 </td>
