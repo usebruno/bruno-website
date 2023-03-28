ae46487afac17da3355649c626290c15df3096c1
Anoop M D <anoop.md1421@gmail.com>
2023-03-29 00:42:46 +0530

    feat: new testimonials <3

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,6 +83,12 @@ export default function Home() {
               <img className="mb-4" src="images/testimonials/david.png" />
             </div>
             <div className="flex flex-col items-center justify-center p-4 bg-white rounded-lg shadow-lg">
+              <img className="mb-4" src="images/testimonials/nesbert.png" />
+            </div>
+            <div className="flex flex-col items-center justify-center p-4 bg-white rounded-lg shadow-lg">
+              <img className="mb-4" src="images/testimonials/DivyMohan.png" />
+            </div>
+            <div className="flex flex-col items-center justify-center p-4 bg-white rounded-lg shadow-lg">
               <img className="mb-4" src="images/testimonials/sudeep.png" />
             </div>
             <div className="flex flex-col items-center justify-center p-4 bg-white rounded-lg shadow-lg">
