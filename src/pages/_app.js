a7440d41e7369894ed2f845c18ac0185460336ee
Anoop M D <anoop.md1421@gmail.com>
2023-10-09 21:40:51 +0530

    chore: vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { ThemeProvider } from 'styled-components';
 import '../styles/globals.css';
 import '../styles/markdown.css';
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   )
 }
