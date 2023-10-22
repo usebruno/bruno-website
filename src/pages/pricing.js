e5078c83497027f03ce7d6d87d153532f6097010
Anoop M D <anoop.md1421@gmail.com>
2023-10-23 02:14:20 +0530

    chore: updated pricing page

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -8,6 +8,12 @@ const CheckIcon = () => {
   return <svg className="flex-shrink-0 w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>;
 };
 
+const HeartIcon = () => {
+  return <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="flex-shrink-0 w-5 h-4 text-yellow-600" viewBox="0 0 16 16">
+    <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+  </svg>;
+};
+
 export default function Pricing() {
 
   const opensourceEdition = [
@@ -82,6 +88,10 @@ export default function Pricing() {
                   <span className="text-gray-500">/year</span>
                 </div>
                 <ul role="list" className="mb-8 space-y-4 text-left">
+                  <li className='flex items-center space-x-3'>
+                    <HeartIcon />
+                    <span>Support Bruno's Development</span>
+                  </li>
                   {goldenEditon.map((item, index) => (
                     <li className="flex items-center space-x-3" key={index}>
                       <CheckIcon />
