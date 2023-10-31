e3eee83be8a3d63c153692a39fcb314cb69de282
Anoop M D <anoop.md1421@gmail.com>
2023-10-31 11:02:36 +0530

    chore: updating landing page copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,10 +21,10 @@ export default function Home() {
           <div className="max-w-screen-xl pt-8 pb-8 mx-auto lg:pt-14">
             <div className="mr-auto place-self-center">
               <h1 className="max-w-3xl mb-10 text-4xl font-bold leading-none tracking-tight md:text-5xl xl:text-6xl">
-                Fast, Git Friendly API Client
+                Re-Inventing the API Client
               </h1>
               <p className="max-w-3xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
-                Bruno is a new and innovative open source API client, aimed at revolutionizing the status quo represented by Postman, Insomnia and similar tools out there. <br /><br />
+                Bruno is a Fast and Git-Friendly Opensource API client, aimed at revolutionizing the status quo represented by Postman, Insomnia and similar tools out there. <br /><br />
                 Bruno stores your collections directly in a folder on your filesystem. We use a plain text markup language, Bru, to save information about API requests. <br /><br />
                 You can use git or any version control of your choice to collaborate over your API collections. <br /><br />
 
