96c354a5b71de5398c54d237fc6361ce9288d9fc
Anoop M D <anoop.md1421@gmail.com>
2023-11-13 14:23:48 +0530

    chore: removed vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,3 @@
-import { Analytics } from '@vercel/analytics/react';
 import { ThemeProvider } from 'styled-components';
 import '../styles/globals.css';
 import '../styles/markdown.css';
@@ -9,7 +8,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
-      <Analytics />
     </ThemeProvider>
   )
 }
