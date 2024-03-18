fc48c161d4d7a44308177c3964b4bc37ccaf8bba
AriBasker <aribaskar.babu@kovai.co>
2024-03-18 19:19:59 +0530

    Fixed

--- src/components/Blogpost/index.js
+++ src/components/Blogpost/index.js
@@ -8,7 +8,7 @@ export default function Layout({ title, keywords, description, children }) {
   return (
     <StyledWrapper className="container flex flex-col root home-page" style={{fontFamily: 'Inter', maxWidth: '1024px'}}>
       <Head>
-        <title>{title}    </title>
+        <title>{title}  fgsrgr  </title>
         <link rel="icon" href="/favicon.ico" />
         <meta name='keywords' content={keywords} />
         <meta name='description' content={description} />
