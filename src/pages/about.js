f2305bc1c1189c6f42ac9ccd9459c6c1bfaf644e
Sanjai Kumar <84461672+sanjai0py@users.noreply.github.com>
2024-05-09 21:43:10 +0530

    Fix/about section (#48)
    * Updated About section.
    
    * Added the images.

--- src/pages/about.js
+++ src/pages/about.js
@@ -91,9 +91,10 @@ function Team({people = []}) {
         <div className="bg-white py-24 sm:py-32">
             <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
                 <div className="max-w-2xl">
-                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our leadership</h2>
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our team</h2>
                     <p className="mt-6 text-lg leading-8 text-gray-600">
-                        We’re a passionate group of individuals who are on a mission to build the Best API Client for Developers
+                        We’re a dynamic group of individuals who are passionate about what we do and dedicated to
+                        delivering the best results for our clients.
                     </p>
                 </div>
                 <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
@@ -137,5 +138,4 @@ function Team({people = []}) {
                 </ul>
             </div>
         </div>
-    )
 }
