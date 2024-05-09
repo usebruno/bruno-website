8bc582bddaaf655ae85e9a5b5513225ef63317a8
sanjai0py <sanjailucifer666@gmail.com>
2024-05-09 21:39:06 +0530

    Added the images.

--- src/pages/about.js
+++ src/pages/about.js
@@ -91,7 +91,7 @@ function Team({people = []}) {
         <div className="bg-white py-24 sm:py-32">
             <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
                 <div className="max-w-2xl">
-                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our leadership</h2>
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our team</h2>
                     <p className="mt-6 text-lg leading-8 text-gray-600">
                         We’re a dynamic group of individuals who are passionate about what we do and dedicated to
                         delivering the best results for our clients.
