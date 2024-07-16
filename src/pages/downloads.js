19cb56a686f41aad36908cb7d1f52fa9b977f131
Anoop M D <anoop.md1421@gmail.com>
2024-07-16 17:18:17 +0530

    chore: bumped version

--- src/pages/downloads.js
+++ src/pages/downloads.js
@@ -15,7 +15,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      latestVersion: '1.20.4',
+      latestVersion: '1.21.0',
       // disabling this for now
       // latestVersion: data.tag_name.replace('v', ''),
       releaseDate: new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: 'numeric' }).format(new Date(data.created_at)),
